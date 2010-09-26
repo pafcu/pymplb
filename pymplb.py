5a3b9b684b55a330b852ccc41549ff13be32e1c3
Stefan Parviainen <pafcu@iki.fi>
2010-09-26 11:41:55 +0300

    Initialize class before any instances are made

--- pymplb.py
+++ pymplb.py
@@ -34,131 +34,149 @@ class MPlayer(object):
 	Available methods and properties are determined at runtime when the class is instantiated. All methods and properties are
 	type-safe and properties respect minimum and maximum values given by mplayer.
 	"""
-	__arg_types = {'Flag':type(False), 'String':type(''), 'Integer':type(0), 'Float':type(0.0), 'Position':type(0.0), 'Time':type(0.0)} # Mapping from mplayer -> Python types
-	def __run_player(self,args):
-		try:
-			player = subprocess.Popen(args,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-		except OSError, e:
-			if e.errno == 2:
-				raise PlayerNotFoundException(args[0])
-			else:
-				raise e
-		return player
+	_arg_types = {'Flag':type(False), 'String':type(''), 'Integer':type(0), 'Float':type(0.0), 'Position':type(0.0), 'Time':type(0.0)} # Mapping from mplayer -> Python types
+	_player_methods = {}
+	_method_prefix = ''
+	_property_prefix = ''
 
-	def __add_methods(self, mplayer_bin):
-		# Function which is run for each mplayer command
-		def cmd(name, argtypes, obligatory, *args, **kwargs):
-			if len(args) < obligatory:
-				raise TypeError('TypeError: %s() takes at least %d arguments (%d given)'%(name,obligatory,len(args)))
-			if len(args) > len(argtypes):
-				raise TypeError('TypeError: %s() takes at most %d arguments (%d given)'%(name,len(argtypes),len(args)))
-			for i in range(len(args)):
-				if type(args[i]) != argtypes[i]:
-					raise TypeError('Argument %d of %s() has type %s, should be %s'%(i,name,type(args[i]).__name__,argtypes[i].__name__))
-			pausing = kwargs.get('pausing','pausing_keep')
-			if pausing != '':
-				pausing = pausing + ' '
-			print >>self.__player.stdin, pausing+name,' '.join((str(x) for x in args))
+	def __init__(self, mplayer_bin='mplayer', mplayer_args_d={}, **mplayer_args):
+		if MPlayer._player_methods == {}:
+			initialize() # Initialize class if it hasn't been done explicitly
 
-			# Read return value of commands that give one
-			# Hopefully this is smart enough ...
-			if name.startswith('get_'):
-				r = self.__player.stdout.readline().split('=',2)[1].rstrip()
-				if r=='PROPERTY_UNAVAILABLE':
-					return None
-				else:
-					return r
-
-		player = self.__run_player([mplayer_bin,'-input','cmdlist'])
-
-		# Add each command found
-		for line in player.stdout:
-			parts = line.strip().split()
-			name = parts[0]
-			args = parts[1:]
-			if len(parts) > 1:
-				obligatory = len([x for x in args if x[0] != '[']) # Number of obligatory args
-				argtypes = [MPlayer.__arg_types[y] for y in [x.strip('[]') for x in args]]
-
-			f = partial(cmd, name, argtypes, obligatory)
-			if len(args) == 0:
-				f.__doc__ = 'Method taking no arguments'
-			elif len(args) == 1:
-				f.__doc__ = 'Method taking argument of type %s'%args[0]
-			else:
-				f.__doc__ = 'Method taking arguments of types %s'%' '.join(args)
-
-			setattr(self.__class__, name, f)
-			
-
-	def __add_properties(self, mplayer_bin):
-		def get_prop(name,p_type,self):
-			# self argument is needed to be property, at the end because of partial
-			r = self.get_property(name)
-			if r != None:
-				if p_type != type(False):
-					r = p_type(r)
-				else:
-					r = r == 'yes'
-			return r
-
-		# Function for getting and setting properties
-		def set_prop(name, p_type, min, max, self, value):
-			if type(value) != p_type:
-				raise TypeError('TypeError: %s has type %s, not %s'%(name,p_type.__name__,type(value).__name__))
-			if min != None and value < min:
-				raise TypeError('TypeError: %s must be at least %s (>%s)'%(name,min,value))
-			if max != None and value > max:
-				raise TypeError('TypeError: %s must be at most %s (<%s)'%(name,max,value))
-
-			self.set_property(name,str(value))
-				
-		player = self.__run_player([mplayer_bin,'-list-properties'])
-		# Add each property found
-		for line in player.stdout:
-			parts = line.strip().split()
-			if len(parts) != 4:
-				continue
-			name = parts[0]
-			try:
-				p_type = MPlayer.__arg_types[parts[1]]
-			except KeyError, e:
-				continue
-				
-			min = parts[2]
-			max = parts[3]
-			if min == 'No':
-				min = None
-			else:
-				min = p_type(min)
-
-			if max == 'No':
-				max = None
-			else:
-				max = p_type(max)
-
-			getter = partial(get_prop, name, p_type)
-			setter = partial(set_prop, name, p_type, min, max)
-			setattr(self.__class__, self.__property_prefix+name, property(getter,setter, doc='Property of type %s in range [%s, %s].'%(p_type.__name__,min,max)))
-
-	def __init__(self, mplayer_bin='mplayer', property_prefix='p_', mplayer_args_d={}, **mplayer_args):
-		self.__property_prefix = property_prefix
-		self.__add_methods(mplayer_bin)
-		self.__add_properties(mplayer_bin)
 		mplayer_args.update(mplayer_args_d)
 		cmd_args = [mplayer_bin,'-slave','-quiet','-idle','-msglevel','all=-1:global=4']
 		for (k,v) in mplayer_args.items():
 			cmd_args.append('-'+k)
 			cmd_args.append(v)
 
-		self.__player = self.__run_player(cmd_args)
+		self.__player = _run_player(cmd_args)
+
+		# Partially apply methods to use the newly created player
+		for (name,f) in MPlayer._player_methods.items():
+			setattr(self,name,partial(f,self.__player))
 
 		atexit.register(self.__cleanup) # Make sure subprocess is killed
 
 	def __cleanup(self):
 		self.__player.terminate()
 
+
+def initialize(mplayer_bin='mplayer',method_prefix='',property_prefix='p_'):
+	MPlayer._method_prefix = method_prefix
+	MPlayer._property_prefix = property_prefix
+	_add_methods(mplayer_bin)
+	_add_properties(mplayer_bin)
+	print MPlayer._method_prefix
+
+def _add_methods(mplayer_bin):
+	# Function which is run for each mplayer command
+	def cmd(name, argtypes, obligatory, player, *args, **kwargs):
+		if len(args) < obligatory:
+			raise TypeError('TypeError: %s() takes at least %d arguments (%d given)'%(name,obligatory,len(args)))
+		if len(args) > len(argtypes):
+			raise TypeError('TypeError: %s() takes at most %d arguments (%d given)'%(name,len(argtypes),len(args)))
+		for i in range(len(args)):
+			if type(args[i]) != argtypes[i]:
+				raise TypeError('Argument %d of %s() has type %s, should be %s'%(i,name,type(args[i]).__name__,argtypes[i].__name__))
+		pausing = kwargs.get('pausing','pausing_keep')
+		if pausing != '':
+			pausing = pausing + ' '
+		print >>player.stdin, pausing+name,' '.join((str(x) for x in args))
+
+		# Read return value of commands that give one
+		# Hopefully this is smart enough ...
+		if name.startswith('get_'):
+			r = player.stdout.readline().split('=',2)[1].rstrip()
+			if r=='PROPERTY_UNAVAILABLE':
+				return None
+			else:
+				return r
+
+	player = _run_player([mplayer_bin,'-input','cmdlist'])
+
+	# Add each command found
+	for line in player.stdout:
+		parts = line.strip().split()
+		name = parts[0]
+		args = parts[1:]
+		if len(parts) > 1:
+			obligatory = len([x for x in args if x[0] != '[']) # Number of obligatory args
+			argtypes = [MPlayer._arg_types[y] for y in [x.strip('[]') for x in args]]
+
+		#f = partial(cmd, self.__player, name, argtypes, obligatory)
+		f = partial(cmd,name,argtypes,obligatory)
+		if len(args) == 0:
+			f.__doc__ = 'Method taking no arguments'
+		elif len(args) == 1:
+			f.__doc__ = 'Method taking argument of type %s'%args[0]
+		else:
+			f.__doc__ = 'Method taking arguments of types %s'%' '.join(args)
+
+		MPlayer._player_methods[MPlayer._method_prefix+name] = f
+		setattr(MPlayer, MPlayer._method_prefix+name, f)
+
+def _add_properties(mplayer_bin):
+	def get_prop(name,p_type,self):
+		# self argument is needed to be property, at the end because of partial
+		r = self.get_property(name)
+		if r != None:
+			if p_type != type(False):
+				r = p_type(r)
+			else:
+				r = r == 'yes'
+		return r
+
+	# Function for getting and setting properties
+	def set_prop(name, p_type, min, max, self, value):
+		if type(value) != p_type:
+			raise TypeError('TypeError: %s has type %s, not %s'%(name,p_type.__name__,type(value).__name__))
+		if min != None and value < min:
+			raise TypeError('TypeError: %s must be at least %s (>%s)'%(name,min,value))
+		if max != None and value > max:
+			raise TypeError('TypeError: %s must be at most %s (<%s)'%(name,max,value))
+
+		self.set_property(name,str(value))
+			
+	player = _run_player([mplayer_bin,'-list-properties'])
+	# Add each property found
+	for line in player.stdout:
+		parts = line.strip().split()
+		if len(parts) != 4:
+			continue
+		name = parts[0]
+		try:
+			p_type = MPlayer._arg_types[parts[1]]
+		except KeyError, e:
+			continue
+			
+		min = parts[2]
+		max = parts[3]
+		if min == 'No':
+			min = None
+		else:
+			min = p_type(min)
+
+		if max == 'No':
+			max = None
+		else:
+			max = p_type(max)
+
+		getter = partial(get_prop, name, p_type)
+		setter = partial(set_prop, name, p_type, min, max)
+		setattr(MPlayer, MPlayer._property_prefix+name, property(getter,setter, doc='Property of type %s in range [%s, %s].'%(p_type.__name__,min,max)))
+
+def _run_player(args):
+	try:
+		player = subprocess.Popen(args,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	except OSError, e:
+		if e.errno == 2:
+			raise PlayerNotFoundException(args[0])
+		else:
+			raise e
+	return player
+
 if __name__ == '__main__':
+	print "foo"
 	p = MPlayer()
 	p.loadfile('test')
 	p.af_add('scaletempo')
