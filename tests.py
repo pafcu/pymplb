dec0aff261ae5dad7cc14f210a33fceae0bb56b8
Stefan Parviainen <pafcu@iki.fi>
2010-10-11 11:26:39 +0300

    More tests

--- tests.py
+++ tests.py
@@ -3,10 +3,13 @@ import time
 import pymplb
 
 class TestMPlayer(unittest.TestCase):
-	def testProperty(self):
+	def testReadProperty(self):
 		player = pymplb.MPlayer()
 		r = player.p_loop
 		self.assertEqual(r,-1)
+
+	def testWriteProperty(self):
+		player = pymplb.MPlayer()
 		player.p_loop = 5
 		r = player.p_loop
 		self.assertEqual(r,5)
@@ -17,11 +20,6 @@ class TestMPlayer(unittest.TestCase):
 		r  = player.get_property('loop')
 		self.assertEqual(r,'1')
 
-	def testNullProperty(self):
-		player = pymplb.MPlayer()
-		r = player.p_filename
-		self.assertEqual(r,None)
-
 	def testListProperty(self):
 		player = pymplb.MPlayer()
 		player.loadfile('test.ogv')
@@ -29,12 +27,15 @@ class TestMPlayer(unittest.TestCase):
 		r = player.p_metadata
 		self.assertEqual(type(r),type([]))
 
+	def testNullProperty(self):
+		player = pymplb.MPlayer()
+		r = player.p_filename
+		self.assertEqual(r,None)
+
 	def testLoadedFileProperties(self):
 		player = pymplb.MPlayer()
-		r = player.p_metadata
-		self.assertEqual(r,None)
 		player.loadfile('test.ogv')
-		r = player.p_metadata
+		r = player.p_filename
 		self.assertNotEqual(r,None)
 
 	def testOtherPrefix(self):
@@ -44,6 +45,29 @@ class TestMPlayer(unittest.TestCase):
 		r = player.m_get_property('loop')
 		self.assertRaises(AttributeError,lambda: player.p_loop)
 		self.assertRaises(AttributeError,lambda: player.get_property('loop'))
+
+	def testMethodTooManyArgs(self):
+		player = pymplb.MPlayer()
+		self.assertRaises(TypeError,lambda: player.get_property('loop','foo'))
+
+	def testMethodTooFewArgs(self):
+		player = pymplb.MPlayer()
+		self.assertRaises(TypeError,lambda: player.get_property())
+
+	def testMethodType(self):
+		player = pymplb.MPlayer()
+		self.assertRaises(TypeError,lambda:player.get_property(0))
+
+	def testSetPropertyType(self):
+		player = pymplb.MPlayer()
+		def f():
+			player.p_loop = '0'
+		self.assertRaises(TypeError,f)
+
+	def testGetpropertyType(self):
+		player = pymplb.MPlayer()
+		r = player.p_loop
+		self.assertEqual(type(r),type(0))
 
 if __name__ == '__main__':
 	unittest.main()
